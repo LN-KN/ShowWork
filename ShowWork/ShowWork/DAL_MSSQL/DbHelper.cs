4ea3ef913bf68b7ad0335f0834cc8fe7b98ed824
Никита <Nikita.Belikov@urfu.me>
2024-04-15 21:26:02 +0500

    Connection string example add

--- ShowWork/ShowWork/DAL_MSSQL/DbHelper.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace ShowWork.DAL_MSSQL
-{
-    public class DbHelper
-    {
-        public static string connString = "Server=DESKTOP-VEVGCI6;Database=ShowWork;Trusted_Connection=True; Encrypt = True; TrustServerCertificate=True";
-    }
-}
